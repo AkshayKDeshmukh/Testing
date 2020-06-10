075b954623d352d63b1eb30ec4536c49a9d66340
AkshayKDeshmukh <66626418+AkshayKDeshmukh@users.noreply.github.com>
2020-06-10 19:47:44 +0530

    Update Buyingproduct.java

--- Buyingproduct.java
+++ Buyingproduct.java
@@ -11,8 +11,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
-
 public class Buyingproduct {
 
 	public static void main(String[] args) {
@@ -22,9 +20,12 @@ public class Buyingproduct {
 		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		// add item to cart
 		addItems(driver);
+		// proceed to checkout the added item
 		checkout(driver);
-		 placeOrder(driver);
+		// Placing the order
+		placeOrder(driver);
 
 	}
 
@@ -36,12 +37,14 @@ public class Buyingproduct {
 			String name = products.get(i).getText();
 			String[] spiltedName = name.split("-");
 			String formattedname = spiltedName[0].trim();
-			List<String> vegatablesNeeded = Arrays.asList(vegitables);
-
+			List<String> vegatablesNeeded = Arrays.asList(vegitables); // coverting array to arraylist at run time
+			// Checking name is present in the array and then clicking on item present in
+			// the arraylist
 			if (vegatablesNeeded.contains(formattedname)) {
 				j++;
 				driver.findElements(By.xpath("//div[@class='product']/div/button")).get(i).click();
 				if (j == vegitables.length) {
+					// Once all items are added to cart come out of the loop
 					break;
 				}
 			}
@@ -54,23 +57,23 @@ public class Buyingproduct {
 		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
 		driver.findElement(By.xpath("//input[@class='promoCode']")).sendKeys("rahulshettyacademy");
 		driver.findElement(By.xpath("//button[@class='promoBtn']")).click();
-		WebDriverWait wt = new WebDriverWait(driver,5);
+		WebDriverWait wt = new WebDriverWait(driver, 5);
 		wt.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='promoInfo']")));
 		String promoCodeInfo = driver.findElement(By.xpath("//span[@class='promoInfo']")).getText();
 		System.out.println(promoCodeInfo);
 		// driver.quit();
-		
 
 	}
-	
-	public static void placeOrder(WebDriver driver)
-	{
+
+	public static void placeOrder(WebDriver driver) {
 		driver.findElement(By.xpath("//button[text()='Place Order']")).click();
-		Select s=new Select(driver.findElement(By.xpath("//div[@class='wrapperTwo']/div/select")));
+		Select s = new Select(driver.findElement(By.xpath("//div[@class='wrapperTwo']/div/select")));
 		s.selectByVisibleText("India");
 		driver.findElement(By.xpath("//input[@class='chkAgree']")).click();
 		driver.findElement(By.xpath("//button[text()='Proceed']")).click();
-		String thanksStatement=driver.findElement(By.xpath("//span[contains(text(),'Thank you, your order has been placed successfully')]")).getText();
+		String thanksStatement = driver
+				.findElement(By.xpath("//span[contains(text(),'Thank you, your order has been placed successfully')]"))
+				.getText();
 		System.out.println(thanksStatement);
 		driver.close();
 	}
